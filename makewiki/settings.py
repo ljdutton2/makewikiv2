de0f8a0408abda4c133ce9f3b6cafcad3a29c942
lauren <ljdutton2@icloud.com>
2019-11-14 16:26:36 -0800

    implement reset password link

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 
 DEFAULT_LOGOUT_URL = '/'
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 
 # Application definition
