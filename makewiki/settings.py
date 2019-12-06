3e12b7798b6943303d4e1c5c74fa94fac089ef4b
lauren <ljdutton2@icloud.com>
2019-12-05 16:22:34 -0800

    fix heroku deploy

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -137,7 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
 
